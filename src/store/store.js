73d05e9d729bdf317110ab06faf5eb856d7119ed
tanishka0123 <guptatanishka2301@gmail.com>
2024-10-19 12:36:36 +0530

    Project

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { login, logout } from "./authSlice";
+
+const store = configureStore({
+  reducer: { login, logout },
+});
+
+export default store;
